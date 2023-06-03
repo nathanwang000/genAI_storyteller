21410548d4e59d888fafbd25951a0f27b07718e3
Jiaxuan Wang <jiaxuanwang@fb.com>
2023-06-03 01:45:56 -0700

    make the process simpler

--- prompts/story_system_prompt1.md
+++ prompts/story_system_prompt1.md
@@ -1,1 +1,1 @@
-The user will ask you to write a children's story.
+The user will ask you to write a children's story. Break your story into paragraphs.
--- story2illustration.py
+++ story2illustration.py
@@ -3,42 +3,32 @@ from lib.utils import img2img, txt2img, save_img
 from lib.utils import ChatBot
 
 
-def parse_story_response(response:str):
-    '''response is a json string: list of story prompt'''
-    res = json.loads(response)
-    for story in res:
-        if 'text' not in story or 'illustration' not in story:
-            raise ValueError('Invalid response')
-    return res
-
 @click.command()
 @click.option('--story_prompt', prompt=True,
               default='Once upon a time, there was a little tiger named lua',
               help='llm prompt')
 @click.option('--output_dir', prompt=True, default='output',
               help='output directory')
-@click.option('--system_prompt_path', prompt=True, default='prompts/story_system_prompt0.md',
+@click.option('--system_prompt_path', prompt=True, default='prompts/story_system_prompt1.md',
               help='system prompt path')
 def main(story_prompt, output_dir, system_prompt_path):
     system_prompt = open(system_prompt_path).read()
     os.system(f'mkdir -p {output_dir}')
     chatbot = ChatBot(system_prompt)
     print('Querying the llm for story completion...')
-    story_prompt = f'Story to complete: {story_prompt}\n\nRemember to respond in json with the correct format!'
+    story_prompt = f"Children story to complete: \n---BEGIN_PROMPT---\n{story_prompt}\n---END_PROMPT---\n\nOutput:"
     response = chatbot(story_prompt)
-    try:
-        r_dict = parse_story_response(response)
-    except Exception as e:
-        print(e)
-        print(response)
-        print('response not formatted correctly, exiting...')
-        return
+    print('Response:\n', response)
 
-    story_fn = os.path.join(output_dir, 'story.json')
-    json.dump(json.loads(response), open(story_fn, 'w'), indent=4)
+
+    story_fn = os.path.join(output_dir, 'story.txt')
+    with open(story_fn, 'w') as f:
+        f.write(response)
+
     print(f'Done generating the story, saved in {story_fn}, starting to generate illustrations...')
-    for i, story in enumerate(tqdm.tqdm(r_dict)):
-        img = txt2img(story['illustration'], "worst-quality, watermark")
+    for i, story in enumerate(tqdm.tqdm([r for r in response.split('\n') if r.strip() != ''])):
+        prompt = story # todo: turn this into keyword for SD to consume
+        img = txt2img(story, "worst-quality, watermark")
         save_img(img,
                  os.path.join(output_dir, f'{i}.png'))
 
