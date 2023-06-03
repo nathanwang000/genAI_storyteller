2faa479e44a5c33480dea8484b443ab5b3c356bc
Jiaxuan Wang <jiaxuanwang@fb.com>
2023-06-03 01:29:53 -0700

    making prompt more robust

---
+++ prompts/story_system_prompt1.md
@@ -0,0 +1,1 @@
+The user will ask you to write a children's story.
--- story2illustration.py
+++ story2illustration.py
@@ -1,12 +1,13 @@
-import click, json, os
+import click, json, os, tqdm
 from lib.utils import img2img, txt2img, save_img
 from lib.utils import ChatBot
+
 
 def parse_story_response(response:str):
     '''response is a json string: list of story prompt'''
     res = json.loads(response)
     for story in res:
-        if 'story' not in story or 'prompt' not in story:
+        if 'text' not in story or 'illustration' not in story:
             raise ValueError('Invalid response')
     return res
 
@@ -16,35 +17,28 @@ def parse_story_response(response:str):
               help='llm prompt')
 @click.option('--output_dir', prompt=True, default='output',
               help='output directory')
-def main(story_prompt, output_dir):
-    system_prompt = '''
-    User will input a story prompt, your job is to output a json file that
-    a) complete the story and
-    b) provide prompt to the stable diffusion model to illustrate the story.
-
-    Break your story in to chunks to create multiple illustrations.
-    The output json should be a list containing the fields "story" and "prompt" where "story" is your completion and "prompt" is fed into stable diffusion.
-
-    Here is an example output
-    ```[{"story": <story chunk 1>, "prompt": <story chunk 1's prompt to stable diffusion>}, {"story": <story chunk 2>, "prompt": <story chunk 2's prompt to stable diffusion>}]```
-    '''
+@click.option('--system_prompt_path', prompt=True, default='prompts/story_system_prompt0.md',
+              help='system prompt path')
+def main(story_prompt, output_dir, system_prompt_path):
+    system_prompt = open(system_prompt_path).read()
     os.system(f'mkdir -p {output_dir}')
     chatbot = ChatBot(system_prompt)
+    print('Querying the llm for story completion...')
+    story_prompt = f'Story to complete: {story_prompt}\n\nRemember to respond in json with the correct format!'
     response = chatbot(story_prompt)
     try:
         r_dict = parse_story_response(response)
     except Exception as e:
         print(e)
+        print(response)
         print('response not formatted correctly, exiting...')
         return
 
     story_fn = os.path.join(output_dir, 'story.json')
-    json.dump(r_dict, open(story_fn, 'w'), indent=4)
-    print(f'done generating the story, saved in {story_fn}')
-    for i, story in enumerate(r_dict):
-        story_text = story['story']
-        prompt = story['prompt']
-        img = txt2img(prompt, "worst-quality, watermark")
+    json.dump(json.loads(response), open(story_fn, 'w'), indent=4)
+    print(f'Done generating the story, saved in {story_fn}, starting to generate illustrations...')
+    for i, story in enumerate(tqdm.tqdm(r_dict)):
+        img = txt2img(story['illustration'], "worst-quality, watermark")
         save_img(img,
                  os.path.join(output_dir, f'{i}.png'))
 
